82b5bdf85022085a92a934626dcb637bb8a4209b
Cyprien-png <cyprien04@hotmail.com>
2025-12-02 17:59:07 +0100

    init: challenge 02

---
+++ 02/index.js
@@ -0,0 +1,16 @@
+import * as fs from 'fs';
+import * as readline from 'readline';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Get input file from command line arguments
+const inputParam = '--input';
+const inputFile = process.argv.includes(inputParam) ? process.argv[process.argv.indexOf(inputParam) + 1] : null;
+
+// Split the input into lines
+const filePath = path.join(__dirname, inputFile);
+const ranges = fs.readFileSync(filePath, 'utf8').trim();
+
+console.log(ranges)
