d984fc4198b4d95815cbde071f837127b5a720d5
Cyprien-png <cyprien04@hotmail.com>
2025-12-02 14:19:38 +0100

    feat: add processing logic for part 2

--- 01/index.js
+++ 01/index.js
@@ -19,6 +19,7 @@ const rl = readline.createInterface({
     crlfDelay: Infinity
 });
 
+// Process with the rules of Part One
 const processPartOne = (line, currentValue) => {
     const mult = line.charAt(0) === 'R' ? 1 : -1;
     const value = parseInt(line.substring(1));
@@ -28,12 +29,31 @@ const processPartOne = (line, currentValue) => {
     return { val, psw };
 }
 
+// Process with the rules of Part Two
+const processPartTwo = (line, currentValue) => {
+    const mult = line.charAt(0) === 'R' ? 1 : -1;
+    const value = parseInt(line.substring(1));
+    
+    let psw = 0;
+    
+    if (currentValue + value * mult === 0) {
+        psw = 1;
+    } else if (mult > 0) {
+        psw = Math.floor((value + currentValue) / 100)
+    } else {
+        psw = Math.floor((currentValue - value) / 100) * -1;
+    }
+    
+    const val = Math.abs(currentValue + mult * value) % 100;
+    return { val, psw };
+}
+
 let password = 0;
 let currentValue = 50;
 
 // Process each line of the input
 rl.on('line', (line) => {
-    const processedLine = (isPartTwo) ? processPartOne(line, currentValue) : processPartOne(line, currentValue);
+    const processedLine = (isPartTwo) ? processPartTwo(line, currentValue) : processPartOne(line, currentValue);
     currentValue = processedLine.val;
     password += processedLine.psw;
 });
