d2073c2873ae4e7af8636a92e826cbcf4978ef80
Cyprien-png <cyprien04@hotmail.com>
2025-12-03 23:53:39 +0100

    refactor: extract the setSequencesLength logic to allow diff with part 2

--- 02/index.js
+++ 02/index.js
@@ -1,5 +1,4 @@
 import * as fs from 'fs';
-import * as readline from 'readline';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -8,6 +7,8 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // Get input file from command line arguments
 const inputParam = '--input';
 const inputFile = process.argv.includes(inputParam) ? process.argv[process.argv.indexOf(inputParam) + 1] : null;
+const partTwoParam = '--part-two';
+const isPartTwo = process.argv.includes(partTwoParam);
 
 // Split the input into lines
 const filePath = path.join(__dirname, inputFile);
@@ -15,15 +16,22 @@ const rangesString = fs.readFileSync(filePath, 'utf8').trim();
 
 let sumOfIds = 0;
 
-rangesString.split(',').map(range => { 
-    const [start, end] = range.split('-').map(num => num);
-    const sequencesLengths = [];
-
-    // Set the all the possible lengths of sequences between start and end
+// Set even sequence lengths only (part one)
+const setSequencesLengthsPartOne = (start, end) => {
+    const seqLen = [];
     for(let i = start.length; i <= end.length; i++) {
         if (i % 2 !== 0) continue;
-        sequencesLengths.push(i / 2);
+        seqLen.push(i / 2);
     }
+    return seqLen;
+}
+
+rangesString.split(',').map(range => { 
+    const [start, end] = range.split('-').map(num => num);
+    if (start.length == 1 && end.length == 1) return;
+
+    // Set the all the possible lengths of sequences between start and end
+    let sequencesLengths = setSequencesLengthsPartOne(start, end);
 
     // Leave if there are no sequence that allows to repeat twice the same subsequence
     if (sequencesLengths.length === 0) return;
