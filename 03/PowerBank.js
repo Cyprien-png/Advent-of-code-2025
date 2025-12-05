e303a1c24afec49b435cc84220aa5874c6186f26
Cyprien-png <cyprien04@hotmail.com>
2025-12-05 13:27:58 +0100

    init challenge 03

---
+++ 03/PowerBank.js
@@ -0,0 +1,25 @@
+export class PowerBank {
+    batteries = [];
+    bestCombination = { first: 0, second: 0 };
+
+    constructor(batteries) {
+        this.batteries = batteries;
+    }
+
+    computeBestCombination() {
+        this.batteries.forEach((b) => {
+            const bVal = parseInt(b);
+
+            if (bVal > this.bestCombination.first) {
+                this.bestCombination.second = this.bestCombination.first;
+                this.bestCombination.first = bVal;
+            } else if (bVal > this.bestCombination.second) {
+                this.bestCombination.second = bVal;
+            }
+
+            if (this.bestCombination.first === 9 && this.bestCombination.second === 9) return this.bestCombination;
+        });
+
+        return this.bestCombination
+    }
+}
---
+++ 03/index.js
@@ -0,0 +1,32 @@
+import * as fs from 'fs';
+import * as readline from 'readline';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { PowerBank } from './PowerBank.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Get input file from command line arguments
+const inputParam = '--input';
+const inputFile = process.argv.includes(inputParam) ? process.argv[process.argv.indexOf(inputParam) + 1] : null;
+
+// Split the input into lines
+const filePath = path.join(__dirname, inputFile);
+const fileStream = fs.createReadStream(filePath);
+const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+});
+
+let password = 0;
+
+// Process each line of the input
+rl.on('line', (line) => {
+    const powerBank = new PowerBank(line.split(''));
+    const comb = powerBank.computeBestCombination();
+    const bankValue = parseInt(`${comb.first}${comb.second}`);
+    password += bankValue;
+});
+
+// Output the result
+rl.on('close', () => console.log(`Password: ${password}`));
