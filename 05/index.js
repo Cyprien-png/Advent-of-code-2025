9167f77a2ec5bdaf22ebe4f192355da18f5cb61e
Cyprien-png <cyprien04@hotmail.com>
2025-12-08 21:33:33 +0100

    feat: add the reduceRange logic in part 2 that is used to merge overlaping ranges

--- 05/index.js
+++ 05/index.js
@@ -8,6 +8,8 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // Get input file from command line arguments
 const inputParam = '--input';
 const inputFile = process.argv.includes(inputParam) ? process.argv[process.argv.indexOf(inputParam) + 1] : null;
+const partTwoParam = '--part-two';
+const isPartTwo = process.argv.includes(partTwoParam);
 
 // Split the input into lines
 const filePath = path.join(__dirname, inputFile);
@@ -23,6 +25,35 @@ const freshIdRanges = [];
 const processIdRange = (rawString) => {
     const range = rawString.trim().split('-');
     freshIdRanges.push({ start: parseInt(range[0]), end: parseInt(range[1]) });
+}
+
+const reduceRanges = () => {
+    let didMergeSomething = false;
+
+    for (let i = 0; i < freshIdRanges.length; i++) {
+        for (let j = 0; j < freshIdRanges.length; j++) {
+            if (i === j) continue;
+
+            // Check if range j is inside range i
+            if (freshIdRanges[j].start >= freshIdRanges[i].start && freshIdRanges[j].end <= freshIdRanges[i].end) {
+                // Remove range j
+                freshIdRanges.splice(j, 1);
+                didMergeSomething = true;
+            } else if (freshIdRanges[j].start >= freshIdRanges[i].start && freshIdRanges[j].start <= freshIdRanges[i].end && freshIdRanges[j].end > freshIdRanges[i].end) {
+                // Adjust the end of range j
+                freshIdRanges[i].end = freshIdRanges[j].end;
+                freshIdRanges.splice(j, 1);
+                didMergeSomething = true;
+            } else if (freshIdRanges[j].start < freshIdRanges[i].start && freshIdRanges[j].end <= freshIdRanges[i].end && freshIdRanges[j].end >= freshIdRanges[i].start) {
+                // Adjust the start of range j
+                freshIdRanges[i].start = freshIdRanges[j].start;
+                freshIdRanges.splice(j, 1);
+                didMergeSomething = true;
+            }
+        }
+    }
+
+    if (didMergeSomething) reduceRanges();
 }
 
 const isFresh = (rawString) => {
@@ -42,9 +73,20 @@ rl.on('line', (line) => {
     if (line.includes('-')) {
         processIdRange(line);
     } else {
-        if (isFresh(line)) password ++;
+        if (!isPartTwo && isFresh(line)) password++;
     }
 });
 
 // Output the result
-rl.on('close', () => console.log(`Password: ${password}`));
+rl.on('close', () => {
+
+    if (isPartTwo) {
+        reduceRanges();
+
+        freshIdRanges.forEach(range => {
+            password += (range.end - range.start + 1);
+        });
+    }
+
+    console.log(`Password: ${password}`);
+});
