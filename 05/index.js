c344f11158c80a43388d8db3b1bcbe5940d25a92
Cyprien-png <cyprien04@hotmail.com>
2025-12-08 20:03:52 +0100

    init: day 05

---
+++ 05/index.js
@@ -0,0 +1,28 @@
+import * as fs from 'fs';
+import * as readline from 'readline';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Get input file from command line arguments
+const inputParam = '--input';
+const inputFile = process.argv.includes(inputParam) ? process.argv[process.argv.indexOf(inputParam) + 1] : null;
+
+// Split the input into lines
+const filePath = path.join(__dirname, inputFile);
+const fileStream = fs.createReadStream(filePath);
+const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+});
+
+let password = 0;
+
+// Process each line of the input
+rl.on('line', (line) => {
+
+});
+
+// Output the result
+rl.on('close', () => console.log(`Password: ${password}`));
