9544688ad7f9cc4e5ab1750d91b37795bf2a9891
Cyprien-png <cyprien04@hotmail.com>
2025-12-13 18:11:04 +0100

    feat: add the method to handle a splitter

--- 07/index.js
+++ 07/index.js
@@ -5,6 +5,9 @@ import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+const TACHYONCHAR = "S";
+const SPLITTERCHAR = "^";
+
 // Get input file from command line arguments
 const inputParam = '--input';
 const inputFile = process.argv.includes(inputParam) ? process.argv[process.argv.indexOf(inputParam) + 1] : null;
@@ -20,9 +23,26 @@ const rl = readline.createInterface({
 let password = 0;
 const currentLineTachyons = [];
 
+// split a Tachyon at the given splitter index
+const splitTachyon = (tachyonIndex, splitterIndex) => {
+    const newTachyonsIndexes = [splitterIndex - 1, splitterIndex + 1];
+    currentLineTachyons.splice(tachyonIndex, 1);
+
+    newTachyonsIndexes.forEach((newIndex) => {
+        if (currentLineTachyons.indexOf(newIndex) === -1) {
+            currentLineTachyons.push(newIndex);
+        }
+    });
+    
+}
+
 // Process each line of the input
 rl.on('line', (line) => {
-    console.log(line)
+    const l = line.split('');
+
+    l.forEach((char, index) => {
+       
+    });
     
 });
 
